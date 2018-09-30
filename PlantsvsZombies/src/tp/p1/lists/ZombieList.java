1f0268a3c834fee97dde5bbabda483b9ff37b31c
Ramón Rosa Pérez <38292919+Ramxnchv@users.noreply.github.com>
2018-09-30 17:12:13 +0200

    Update ZombieList.java

--- PlantsvsZombies/src/tp/p1/lists/ZombieList.java
+++ PlantsvsZombies/src/tp/p1/lists/ZombieList.java
@@ -1,5 +1,38 @@
 package tp.p1.lists;
 
-public class ZombieList {
+import tp.p1.zombies.Zombie;
 
+public class ZombieList {
+	private Zombie[] list;
+	private static final int NUM_MAX_ZOMBIES=10;
+	
+	public ZombieList() {
+		list=new Zombie[NUM_MAX_ZOMBIES];
+	}
+	
+	public boolean Add(Zombie zombie) {
+		if(list.length<NUM_MAX_ZOMBIES) {
+			list[list.length]=zombie;
+		}
+		return list.length<NUM_MAX_ZOMBIES;
+	}
+	public boolean Delete(int pos) {
+		for(int i=pos;i<list.length;i++) {
+			list[i]=list[i+1];
+		}
+		return true;
+	}
+	public int searchPosition(int x,int y) {
+		int i=0;
+		boolean encontrado=false;
+		while(i<list.length&&!encontrado) {
+			if((list[i].getX()==x)&&(list[i].getY()==y)) {
+				encontrado=true;
+			}
+			else {
+			i++;	
+			}
+		}
+		return i;
+	}
 }
