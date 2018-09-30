81a4824ddef617144e93f91f3e52f06fa3867773
Ramón Rosa Pérez <38292919+Ramxnchv@users.noreply.github.com>
2018-09-30 17:11:26 +0200

    Update PeaShooterList.java

--- PlantsvsZombies/src/tp/p1/lists/PeaShooterList.java
+++ PlantsvsZombies/src/tp/p1/lists/PeaShooterList.java
@@ -1,5 +1,38 @@
 package tp.p1.lists;
 
-public class PeaShooterList {
+import tp.p1.plants.PeaShooter;
 
+public class PeaShooterList {
+	private PeaShooter[] list;
+	private static final int TAMAÑO_TABLERO=32;
+	
+	public PeaShooterList() {
+		list=new PeaShooter[TAMAÑO_TABLERO];
+	}
+	
+	public boolean Add(PeaShooter peashooter) {
+		if(list.length<TAMAÑO_TABLERO) {
+			list[list.length]=peashooter;
+		}
+		return list.length<TAMAÑO_TABLERO;
+	}
+	public boolean Delete(int pos) {
+		for(int i=pos;i<list.length;i++) {
+			list[i]=list[i+1];
+		}
+		return true;
+	}
+	public int searchPosition(int x,int y) {
+		int i=0;
+		boolean encontrado=false;
+		while(i<list.length&&!encontrado) {
+			if((list[i].getX()==x)&&(list[i].getY()==y)) {
+				encontrado=true;
+			}
+			else {
+			i++;	
+			}
+		}
+		return i;
+	}
 }
